6346717cb9c6048828c4326b57ef11baa4fe6017
Evgeniy Shevtsov <yevgeniy.shevtsov.pblcb@mail.ru>
2024-05-24 01:40:01 +0300

    feat: add link

---
+++ script.js
@@ -0,0 +1,8 @@
+const locationInfo = document.querySelector("#location");
+
+locationInfo.addEventListener("click", () => {
+  window.open(
+    "https://yandex.by/maps/29632/brest-district/house/Zk8YdAZoTkUBQFtofXx5dHlqZg==/?ll=26.237429%2C52.087679&mode=search&sctx=ZAAAAAgBEAAaKAoSCXmu78NBjjtAES%2F6CtKM80pAEhIJ%2BWcG8YEd1z8R295uSQ7YvT8iBgABAgMEBSgKOABAzZ0GSAFqAnVhnQHNzEw9oAEAqAEAvQGkchYcwgEGsMD1wrsEggIu0LDQs9GA0L7Rg9GB0LDQtNGM0LHQsCDQvtGA0LXRiNC10Log0L%2FQuNC90YHQuooCAJICBTIxNTEwmgIMZGVza3RvcC1tYXBz&sll=26.237429%2C52.087679&sspn=0.211800%2C0.071313&text=%D0%B0%D0%B3%D1%80%D0%BE%D1%83%D1%81%D0%B0%D0%B4%D1%8C%D0%B1%D0%B0%20%D0%BE%D1%80%D0%B5%D1%88%D0%B5%D0%BA%20%D0%BF%D0%B8%D0%BD%D1%81%D0%BA&z=12.77",
+    "_blank"
+  );
+});
